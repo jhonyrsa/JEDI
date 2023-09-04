8f9466fd4311d303a06210fc4cc6b7c67543fb94
Jhony Almeida <131838700+jhonyrsa@users.noreply.github.com>
2023-09-03 22:18:12 -0300

    JEDI

---
+++ Modulo1_Introducao-programacao-I/Licao03/ex01_melhorandoHelloWorld/src/melhorandoHelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Jhony Almeida <jhonyrsa@gmail.com>
+ */
+public class melhorandoHelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Welcome to Java Programming");
+        System.out.print("Welcome\nto\nJava\nProgramming\n");
+    }
+    
+}
---
+++ Modulo1_Introducao-programacao-I/Licao03/ex02_a-arvore/src/aArvore.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Jhony Almeida <jhonyrsa@gmail.com>
+ */
+public class aArvore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("I think that I shall never see,\n"+
+                "[Eu acho que nunca verei,]\n"+
+                "a poem as lovely as a tree.\n"+
+                "[um poema tao adoravel quanto uma arvore.]\n"+
+                "A tree whose hungry mouth is pressed\n"+
+                "[Uma arvore cuja boca faminta eh pressionada]\n"+
+                "Against the Earth's sweet flowing breast.\n"+
+                "[Contra a Terra fluindo em seu seio docemente.]\n");
+    }
+    
+}
