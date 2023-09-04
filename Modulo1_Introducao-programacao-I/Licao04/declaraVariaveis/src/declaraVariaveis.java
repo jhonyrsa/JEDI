0ebd1eb65f9241ec6834a0209936aa8919a02d1e
Jhony Almeida <131838700+jhonyrsa@users.noreply.github.com>
2023-09-03 22:25:28 -0300

    JEDI

---
+++ Modulo1_Introducao-programacao-I/Licao04/declaraVariaveis/src/declaraVariaveis.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Jhony Almeida <jhonyrsa@gmail.com>
+ */
+public class declaraVariaveis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //declara uma variável do tipo boolean
+        boolean resultado;
+        char option;
+        option = 'C';
+        double grade = 0.0;
+    }
+    
+}
